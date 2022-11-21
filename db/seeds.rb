401063c829bd828db9fbee0c9ae6a0107883c71d
Sara Raley <sara.m.raley@gmail.com>
2022-11-21 14:09:47 -0700

    creates seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!(name: "Keosha", email: "k@example.com", password: "password", password_confirmation: "password", image: "placeholder")
+User.create!(name: "Hannah", email: "h@example.com", password: "password", password_confirmation: "password", image: "placeholder")
+User.create!(name: "Christine", email: "c@example.com", password: "password", password_confirmation: "password", image: "placeholder")
