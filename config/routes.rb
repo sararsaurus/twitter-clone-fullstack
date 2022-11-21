768936309248b7fe0b0766737dae32fb460595c8
Sara Raley <sara.m.raley@gmail.com>
2022-11-21 13:24:52 -0700

    creates users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  #TRIVIAL COMMENT
-  #CATS ARE fine
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
