40a76506d62d05a10d6fd04b1d612dd0e72d868a
hannahekehoe <hkehoe90@gmail.com>
2022-11-22 14:09:38 -0600

    did a lot of unpleasant things like deleting relationships model and follows table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,4 +52,16 @@ class UsersController < ApplicationController
     @user.destroy
     redirect_to "/users", status: :see_other
   end
+
+  def follow
+    @user = User.find_by(params[:id])
+    current_user.followees << @user
+    redirect_back(fallback_location: user_path(@user))
+  end
+
+  def unfollow
+    @user = User.find(params[:id])
+    current_user.followed_users.find_by(followee_id: @user.id).destroy
+    redirect_back(fallback_location: user_path(@user))
+  end
 end
